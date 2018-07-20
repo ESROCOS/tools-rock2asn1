096195cf6335a1e540cb8be59bafcdaf067ca64b
Raquel Jalvo <rjalvo@gmv.com>
2018-07-20 15:00:58 +0200

    Modified test_base_support in order to execute it after ./test_generate.sh

--- test/test_base_support.sh
+++ test/test_base_support.sh
@@ -1,63 +1,29 @@
-echo "Test for base_support"
-echo ""
+if [ -d out_support ]; then 
+    
+    cd out_support/src
 
-PYTHONPATH=../python:$PYTHONPATH
+    echo "Compiling test function"
+    cp ../../src_support/*.cpp .
+    cp ../../src_support/*.hpp .
 
-echo "Clear old results"
-rm -rf out_asn out_c out_support
-mkdir -p out_asn out_c out_support
-echo "Done."
-echo ""
+    #g++ -I. -fPIC `pkg-config --cflags base-transport-typelib-gnulinux --cflags eigen3 --cflags base-types` -c main.cpp test_frame.cpp
+    #g++ -L. -L${AUTOPROJ_CURRENT_ROOT}/install/lib -lbase-types -ltest_support -o main main.o test_frame.o
 
-echo "Run generation"
-../python/rock2asn1_generate.py base.tlb out_asn out_support
-echo "Done."
-echo ""
+    g++ -I. -fPIC `pkg-config --cflags base-transport-typelib-gnulinux --cflags eigen3 --cflags base-types` -c test.cpp
+    g++ -L. -L${AUTOPROJ_CURRENT_ROOT}/install/lib -lbase-types -ltest_support -lboost_system -lboost_thread -lboost_unit_test_framework -o test test.o
 
-echo "Copying modified userdefs-base.asn to execute the unit tests for base_support"
-cp src_support/userdefs-base.asn out_asn/asn/
-
-
-echo "Run ASN.1 compilation to C"
-asn1.exe -c -o out_c -uPER --type-prefix asn1Scc -atc out_asn/asn/* 
-echo "Done."
-echo ""
-
-
-echo "Compile C code"
-cd out_c
-make
-cd ..
-echo "Done."
-echo ""
-
-
-echo "Compile conversion functions (this may take a while)"
-cd out_support/src
-ln -s ../../out_c asn1
-g++ -fPIC `pkg-config --cflags base-transport-typelib-gnulinux --cflags eigen3 --cflags base-types` -c *.cpp asn1/*.c
-g++ -shared -o libtest_support.so *.o
-if [ ! -f libtest_support.so ]; then
-    echo "Build failed!"
+    if [ -f test ]; then
+        echo "Executing test function"
+        ./test
+    else
+        echo "Test build  failed!"
+    fi
+    cd ../..
+    echo "Done."
+    echo ""
+else    
+    echo "Please generate asn library and support library running ./test_generate.sh "
+    echo "Test Failed."
+    echo ""
 fi
-
-echo "Compile main function"
-cp ../../src_support/*.cpp .
-cp ../../src_support/*.hpp .
-
-#g++ -I. -fPIC `pkg-config --cflags base-transport-typelib-gnulinux --cflags eigen3 --cflags base-types` -c main.cpp test_frame.cpp
-#g++ -L. -L${AUTOPROJ_CURRENT_ROOT}/install/lib -lbase-types -ltest_support -o main main.o test_frame.o
-
-g++ -I. -fPIC `pkg-config --cflags base-transport-typelib-gnulinux --cflags eigen3 --cflags base-types` -c test.cpp
-g++ -L. -L${AUTOPROJ_CURRENT_ROOT}/install/lib -lbase-types -ltest_support -lboost_system -lboost_thread -lboost_unit_test_framework -o test test.o
-
-if [ -f test ]; then
-    echo "Executing test function"
-    ./test
-else
-    echo "Test build  failed!"
-fi
-cd ../..
-echo "Done."
-echo ""
 
